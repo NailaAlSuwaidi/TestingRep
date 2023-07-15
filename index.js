cf11f939888c112864acc3f60676f6ca3afd2e93
NailaAlSuwaidi <139650162+NailaAlSuwaidi@users.noreply.github.com>
2023-07-15 23:29:46 +0400

    Update index.js
    changed port number

--- index.js
+++ index.js
@@ -54,5 +54,5 @@ app.post('/api/courses', (req, res)=>{
         
         
 //PORT
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`listening on port ${port}`));
